57d17799f910415f1bdc7796eca055934c356ee4
Itay Bachar <itay.bachar01@gmail.com>
2022-12-25 17:07:26 -0500

    Added moving background

--- scripts/background.js
+++ scripts/background.js
@@ -1,17 +1,20 @@
 let background_x = 0;
 let background_y = 0;
 
-const windowWidth = window.innerWidth / 5;
-const windowHeight = window.innerHeight / 5;
+
 const background = document.getElementById("background");
 
-background.addEventListener('mousemove', (e) =>
-{
-    const mouseX = e.clientX / windowWidth;
-    const mouseY = e.clientY / windowHeight;
+const isMobile = window.matchMedia("(pointer:coarse)").matches;
 
-
-    background.style.backgroundPosition = `translateX(${mouseX})`; 'backgroundX: -30px';// = mouseX; //`translate3d(-${mouseX}%, -${mouseY}%, 0)`;
-});
+if (!isMobile) {
+    document.body.addEventListener('mousemove', (e) => {
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
+        const movePower = 5;
+        const mouseX = movePower * (e.pageX - windowWidth / 2) / windowWidth / 2;
+        const mouseY = movePower * (e.pageY - windowHeight / 2) / windowHeight / 2;
+        background.style.transform = `translate3d(${mouseX}%, ${mouseY}%, 0)`;
+    });
+}
 
 // document.onload = startBackground();
