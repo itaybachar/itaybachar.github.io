2dc47e5ca5713f0cf33652420910bb91329d31ca
Itay Bachar <itay.bachar01@gmail.com>
2022-12-26 11:13:29 -0500

    aesthetic changes,git status

--- scripts/settings.js
+++ scripts/settings.js
@@ -3,9 +3,36 @@ function toggleFlying(e)
     if (e.checked)
     {
         startMovement();
+        document.getElementById("background").classList.add("moving");
     } else
     {
         stopMovement();
+        document.getElementById("background").classList.remove("moving");
     }
-    console.log(e.checked)
+}
+
+function resetToStartScreen()
+{
+    if (document.getElementById("title-name").classList.contains("small"))
+    {
+
+        stopMovement();
+        document.getElementById("background").classList.add("moving");
+
+        document.getElementById("title-name").classList.remove("small");
+        document.getElementById("start-container").classList.remove("hide");
+        document.getElementById("flying-setting").classList.add("hide");
+
+        //uncheck flying
+        document.getElementById("flying-input").checked = false;
+
+        //Check if its animating the opening
+        if (terrainStartAnimator)
+        {
+            clearInterval(terrainStartAnimator);
+            terrainStartAnimator = undefined;
+        }
+        clearFrame();
+    }
+
 }
