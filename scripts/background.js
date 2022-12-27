e2b4aaba337c015ba4ab1cfd3f4ae32710c4eae3
Itay Bachar <itay.bachar01@gmail.com>
2022-12-26 21:35:28 -0500

    small aesthetic changes to background movement

--- scripts/background.js
+++ scripts/background.js
@@ -6,18 +6,23 @@ const background = document.getElementById("background");
 
 const isMobile = window.matchMedia("(pointer:coarse)").matches;
 
-if (!isMobile) {
-    document.body.addEventListener('mousemove', (e) => {
+if (!isMobile)
+{
+    document.body.addEventListener('mousemove', (e) =>
+    {
         const windowWidth = window.innerWidth;
         const windowHeight = window.innerHeight;
         const movePower = 5;
         const mouseX = movePower * (e.pageX - windowWidth / 2) / windowWidth / 2;
-        const mouseY = movePower * (e.pageY - windowHeight / 2) / windowHeight / 2;
+        let mouseY = movePower * (e.pageY - windowHeight / 2) / windowHeight / 2;
+
+        if (background.classList.contains("moving"))
+            mouseY = 0;
         background.style.transform = `translate3d(${mouseX}%, ${mouseY}%, 0)`;
 
-        if(canvas)
+        if (canvas)
         {
-            canvas.style.transform = `translate3d(${0.3*mouseX}%, ${0.5*mouseY}%, 0)`;
+            canvas.style.transform = `translate3d(${0.3 * mouseX}%, ${0.5 * mouseY}%, 0)`;
         }
     });
 }
